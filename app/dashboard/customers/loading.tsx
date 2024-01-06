a5c12f3ec74986eea894f7803513c9a296eec962
dafu <xiangbingze@wenru.wang>
2024-01-06 21:47:12 +0800

    shadcn 导入成功

--- app/dashboard/customers/loading.tsx
+++ app/dashboard/customers/loading.tsx
@@ -1,5 +1,7 @@
-// import DashboardSkeleton from '@/app/ui/skeletons';
-//
-// export default function Loading() {
-//     return <DashboardSkeleton />;
-// }
+import DashboardSkeleton from '@/app/ui/skeletons';
+
+export default function Loading() {
+    return <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Taxing Laughter: The Joke Tax Chronicles
+    </h1>
+}
--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,45 @@
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/app/ui/alert-dialog";
+import {Button} from "@/app/ui/components/button";
+
+
 export default function Page() {
-    return <p>Customers Page</p>;
+    return(
+    <div>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Taxing Laughter: The Joke Tax Chronicles
+        </h1>
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+            The People of the Kingdom
+        </h2>
+         <Button>Button</Button>
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                <Button variant="outline">Show Dialog</Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action cannot be undone. This will permanently delete your
+                        account and remove your data from our servers.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction>Continue</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    </div>)
+
 }
